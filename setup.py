af521af3c744a859632439646beb0b4268968ccc
Bhagyesh Vikani <bhagyesh@infocusp.in>
2017-11-02 12:56:00 +0530

    #18

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ except ImportError:
 from distutils.command.clean import clean
 
 def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	if six.PY2:
+		return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	else:
+		return open(os.path.join(os.path.dirname(__file__), fname), encoding='latin1').read()
 
 class CleanCommand(clean):
 	"""Custom clean command to tidy up the project root."""
